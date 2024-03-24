94c24018e8ac63ed7a6e0357093fa8e59844641d
Sarahjl <sarah.laurindo@etec.sp.gov.br>
2024-03-24 01:10:45 -0300

    arquivos do projeto

---
+++ models/post.js
@@ -0,0 +1,22 @@
+const db = require("./banco")
+
+const Agendamentos = db.sequelize.define('agendamentos',{
+    nome:{
+        type: db.Sequelize.STRING
+    },
+    telefone:{
+        type: db.Sequelize.STRING
+    },
+    origem:{
+        type: db.Sequelize.STRING
+    },
+    data_contato:{
+        type: db.Sequelize.STRING
+    },
+    observacao:{
+        type: db.Sequelize.STRING
+    },
+})
+
+// Agendamentos.sync({force: true})
+
